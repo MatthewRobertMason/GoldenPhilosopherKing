27df674c328f9ea32ffe15e1870be71c5d158b91
Adam Douglass <douglass@malloc.ca>
2020-10-03 13:16:22 -0400

    Add trolley driver

---
+++ GoldenPhilosopherKing/Assets/Scripts/TrolleyDriver.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum TrackSegment {
+    Entering, BottomRight, ExitRight, Top, ExitLeft, BottomLeft
+}
+
+public class TrolleyDriver : MonoBehaviour
+{
+
+    public Sprite MovingUp;
+    public Sprite MovingRight;
+    public Sprite MovingDown;
+    public Sprite MovingLeft;
+
+    public Transform EnterSegment;
+    public Transform ExitRightSegment;
+    public Transform ExitLeftSegment;
+    public Transform TopSegment;
+    public Transform BottomRightSegment;
+    public Transform BottomLeftSegment;
+
+    public bool ExitRight = false;
+    public bool ExitLeft = false;
+
+    private TrackSegment currentSegment = TrackSegment.Entering;
+    private Transform currentPathObject = null;
+    private float segmentDistance = 0;
+    private float segmentSpeed = 1;
+
+    public GameObject Trolley;
+
+    void Start(){
+        currentPathObject = EnterSegment;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        segmentDistance += Time.deltaTime * segmentSpeed;
+        if(segmentDistance >= 1){
+            switch(currentSegment){
+                case TrackSegment.Entering:
+                case TrackSegment.BottomLeft:
+                    currentSegment = TrackSegment.BottomRight;
+                    currentPathObject = BottomRightSegment;
+                    break;
+
+                case TrackSegment.BottomRight:
+                    if(ExitRight){
+                        currentSegment = TrackSegment.ExitRight;
+                        currentPathObject = ExitRightSegment;
+                    } else {
+                        currentSegment = TrackSegment.Top;
+                        currentPathObject = TopSegment;
+                    }
+                    break;
+
+                case TrackSegment.Top:
+                    if(ExitLeft){
+                        currentSegment = TrackSegment.ExitLeft;
+                        currentPathObject = ExitLeftSegment;
+                    } else {
+                        currentSegment = TrackSegment.BottomLeft;
+                        currentPathObject = BottomLeftSegment;
+                    }
+                    break;
+
+                case TrackSegment.ExitLeft: 
+                case TrackSegment.ExitRight:
+                    Debug.Log("TODO Reset Scene to new board");
+                    return;
+            }
+            segmentDistance = 0;
+        }
+
+        DrawTrolley();
+    }
+
+    void DrawTrolley(){
+        Vector2 p0 = currentPathObject.GetChild(0).position;
+        Vector2 p1 = currentPathObject.GetChild(1).position;
+        Vector2 p2 = currentPathObject.GetChild(2).position;
+        Vector2 p3 = currentPathObject.GetChild(3).position;
+
+        Vector2 oldPosition = Trolley.transform.position;
+
+        Trolley.transform.position = Mathf.Pow(1 - segmentDistance, 3) * p0 + 
+            3 * Mathf.Pow(1 - segmentDistance, 2) * segmentDistance * p1 + 
+            3 * (1 - segmentDistance) * Mathf.Pow(segmentDistance, 2) * p2 + 
+            Mathf.Pow(segmentDistance, 3) * p3;
+
+        Vector2 delta = (Vector2)Trolley.transform.position - oldPosition;
+        if(Mathf.Abs(delta.x) > Mathf.Abs(delta.y)){
+            if(delta.x > 0){
+                DrawMovingRight();
+            } else {
+                DrawMovingLeft();
+            }
+        } else {
+            if(delta.y > 0){
+                DrawMovingUp();
+            } else {
+                DrawMovingDown();
+            }
+        }
+    }
+
+    void DrawMovingUp(){
+        Trolley.GetComponent<SpriteRenderer>().sprite = MovingUp;
+    }
+
+    void DrawMovingRight(){
+        Trolley.GetComponent<SpriteRenderer>().sprite = MovingRight;        
+    }
+
+    void DrawMovingDown(){
+        Trolley.GetComponent<SpriteRenderer>().sprite = MovingDown;        
+    }
+
+    void DrawMovingLeft(){
+        Trolley.GetComponent<SpriteRenderer>().sprite = MovingLeft;
+    }
+}
