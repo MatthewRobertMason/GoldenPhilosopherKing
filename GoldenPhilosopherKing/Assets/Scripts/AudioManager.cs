55d5f9211032454eedd05203c6cb0d699df8edb0
MatthewRobertMason <Git@matthewmason.ca>
2020-10-03 18:31:35 -0300

    Audio Manager Stuff

--- GoldenPhilosopherKing/Assets/Scripts/AudioManager.cs
+++ GoldenPhilosopherKing/Assets/Scripts/AudioManager.cs
@@ -7,11 +7,22 @@ public class AudioManager : MonoBehaviour
 {
     private AudioSource audioSource;
 
+    private int currentTrack = 0;
     public AudioClip[] music;
 
     public AudioClip startAudio;
 
-    
+    [Range(0.0f, 30.0f)]
+    public float fadeTime = 10.0f;
+
+    [Range(0.0f, 60.0f)]
+    public float audioTrackCutOff = 5.0f;
+
+    [Range(0.0f, 1.0f)]
+    public float volumeMultiplier = 1.0f;
+
+    [Range(0.0f, 1.0f)]
+    public float volume = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +43,27 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        audioSource.volume = volume * volumeMultiplier;
+
+        if (audioSource.time == audioSource.clip.length)
+        {
+            // Next song
+            audioSource.clip = music[((++currentTrack) % music.Length)];
+            audioSource.Play();
+        }
+
+        if (fadeTime > 0)
+        { 
+            if (audioSource.time > audioSource.clip.length - fadeTime)
+            {
+                audioSource.volume = volume * volumeMultiplier * Mathf.Lerp(1.0f, 0.0f, (audioSource.time - (audioSource.clip.length - fadeTime)) / fadeTime);
+            }
+
+            if (audioSource.time < fadeTime)
+            {
+                audioSource.volume = volume * volumeMultiplier * Mathf.Lerp(0.0f, 1.0f, (audioSource.time / fadeTime));
+            }
+        }
     }
 
     public void StartMusic()
@@ -57,8 +88,13 @@ public class AudioManager : MonoBehaviour
         audioSource.Pause();
     }
 
-    public void SetVolume()
+    public void SetVolume(float setVolume)
     {
+        volume = setVolume;
+    }
 
+    public void SetVolumeModifier (float multiplier)
+    {
+        volumeMultiplier = multiplier;
     }
 }
