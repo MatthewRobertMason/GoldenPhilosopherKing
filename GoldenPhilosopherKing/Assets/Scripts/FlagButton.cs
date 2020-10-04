3d442daa414ba42bb7df691b0b366751933e7826
Adam Douglass <douglass@malloc.ca>
2020-10-04 13:13:04 -0400

    Add semaphore buttons

---
+++ GoldenPhilosopherKing/Assets/Scripts/FlagButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagButton : MonoBehaviour
+{
+    public TrolleyDriver Trolley;
+    public bool TurnLeft = true;
+
+    void OnMouseDown(){
+        if(TurnLeft) Trolley.TurnLeft();
+        else Trolley.TurnRight();
+        this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+}
