89784cd0ed8b9b59bcc87b3429d2f9a15d92a7a4
Adam Douglass <douglass@malloc.ca>
2020-10-04 20:41:17 -0400

    Add menu transition + invisible hand to scene

---
+++ GoldenPhilosopherKing/Assets/Scripts/MenuTransition.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTransition : MonoBehaviour
+{
+    private float transitionElapsed = 0;
+    private bool moving = true;
+    public float TransitionDuration = 3;
+    private Vector2 transitionStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transitionStart = this.gameObject.transform.position;
+        moving = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moving){
+            transitionElapsed += Time.deltaTime;
+            float t = transitionElapsed/TransitionDuration;
+
+            if(t < 1){
+                var path = new Vector2(0, 0) - transitionStart;
+
+                this.gameObject.transform.position = transitionStart + path * t;
+
+            } else {
+                this.gameObject.transform.position = new Vector2(0, 0);
+                moving = false;
+            }
+        }
+    }
+}
