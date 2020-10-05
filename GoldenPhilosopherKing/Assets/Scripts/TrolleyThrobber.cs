a13e2e1e0af59a8b6b1901123c8b8a862c855efa
Adam Douglass <douglass@malloc.ca>
2020-10-04 20:01:31 -0400

    Add shadowbox and trolley loop

---
+++ GoldenPhilosopherKing/Assets/Scripts/TrolleyThrobber.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrolleyThrobber : MonoBehaviour
+{
+
+    float initialX;
+    public float Speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialX = this.gameObject.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.Translate(new Vector2(Time.deltaTime * Speed, 0));
+
+        if(this.gameObject.transform.position.x > -initialX){
+            this.gameObject.transform.position = new Vector2(
+                initialX,
+                this.gameObject.transform.position.y
+            );
+        }
+    }
+}
